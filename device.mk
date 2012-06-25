f4c02b2aa1f2eab20eaac2e04d03c81abe6270c0
genokolar <genokolar@gmail.com>
2012-06-25 12:23:37 +0800

    clean up init.huawei.rc & fixed wifi

--- device.mk
+++ device.mk
@@ -63,7 +63,7 @@ PRODUCT_COPY_FILES += \
     device/huawei/u8818/prebuilt/ueventd.huawei.rc:root/ueventd.huawei.rc \
     device/huawei/u8818/prebuilt/init.qcom.sh:root/init.qcom.sh \
     device/huawei/u8818/prebuilt/init.huawei.usb.rc:root/init.huawei.usb.rc
- #   device/huawei/u8818/prebuilt/init.rc:root/init.rc
+#   device/huawei/u8818/prebuilt/init.rc:root/init.rc
 
 PRODUCT_COPY_FILES += \
     device/huawei/u8818/prebuilt/system/wifi/dhd_4330.ko:system/wifi/dhd_4330.ko \
