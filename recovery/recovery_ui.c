753c2d1d57a24f2da969e869401f8b0b03f26e8b
Lalit Maganti <lalitmaganti@gmail.com>
2012-06-29 22:46:02 +0200

    Fix build

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -20,24 +20,6 @@
 #include "common.h"
 #include "extendedcommands.h"
 
-char* MENU_HEADERS[] = { "Use vol keys to highlight and home to select.",
-			 "",
-                         NULL };
-
-char* MENU_ITEMS[] = { "reboot system now",
-                       "apply sdcard:update.zip",
-                       "wipe data/factory reset",
-                       "wipe cache partition",
-                       "install zip from sdcard",
-                       "backup and restore",
-                       "mounts and storage",
-                       "advanced",
-                       NULL };
-
-int device_recovery_start() {
-    return 0;
-}
-
 int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
     if (alt && key_code == KEY_L)
@@ -47,10 +29,6 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_END);
     }
     return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
-}
-
-int device_reboot_now(volatile char* key_pressed, int key_code) {
-    return 0;
 }
 
 int device_handle_key(int key_code, int visible) {
