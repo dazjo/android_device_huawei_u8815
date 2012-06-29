f65e750be3dd703c95fe6315c17438686f69ab94
Daz Jones <yuki@thebronasium.com>
2012-06-29 20:20:33 +0100

    Hack for the Huawei RIL copying. lmao look at this guy

--- path/readme.txt
+++
@@ -1,6 +0,0 @@
-create 
-frameworks/base/telephony/java/com/android/internal/telephony/HWQcomRIL.java
-
-../system.prop  
-add 
-ro.telephony.ril_class=HWQcomRIL
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
+# Best hack ever.
+cp path/HWQcomRIL.java ../../../frameworks/base/telephony/java/com/android/internal/telephony
 add_lunch_combo huawei_u8818-userdebug
 add_lunch_combo cm_u8818-userdebug
