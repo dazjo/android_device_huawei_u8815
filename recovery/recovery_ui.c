9ad50e3267f06460fbfd4c792f0b1e8728587e3b
Daz Jones <yuki@thebronasium.com>
2012-06-29 23:16:18 +0100

    Update recovery

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -26,9 +26,9 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 1;
     // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
     if (ui_get_showing_back_button()) {
-        return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_END);
+        return get_allow_toggle_display() && (key_code == KEY_MENU);
     }
-    return get_allow_toggle_display() && (key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
+    return get_allow_toggle_display() && (key_code == KEY_MENU);
 }
 
 int device_handle_key(int key_code, int visible) {
@@ -48,15 +48,14 @@ int device_handle_key(int key_code, int visible) {
                     return GO_BACK;
                 break;
 
-	    case KEY_HOME:
+	        case KEY_HOME:
                 return SELECT_ITEM;
-            
+
             case KEY_BACK:
                 if (!get_allow_toggle_display())
                     return GO_BACK;
         }
     }
-
     return NO_ACTION;
 }
 
