f469831650239b57b959219ed62c591754d47848
Daz Jones <yuki@thebronasium.com>
2012-07-21 16:02:52 +0100

    Fix

--- device.mk
+++ device.mk
@@ -63,7 +63,7 @@ PRODUCT_COPY_FILES += \
     device/huawei/u8818/prebuilt/init.huawei.rc:root/init.huawei.rc \
     device/huawei/u8818/prebuilt/ueventd.huawei.rc:root/ueventd.huawei.rc \
     device/huawei/u8818/prebuilt/init.qcom.sh:root/init.qcom.sh \
-	device/huawei/u8818/prebuilt/system/lib/hw/gralloc.u8818.so:system/lib/hw/gralloc.u8818.so \
+    device/huawei/u8818/prebuilt/system/lib/hw/gralloc.u8818.so:system/lib/hw/gralloc.u8818.so \
     device/huawei/u8818/prebuilt/init.huawei.usb.rc:root/init.huawei.usb.rc
 
 PRODUCT_COPY_FILES += \
