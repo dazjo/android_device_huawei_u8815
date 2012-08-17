c88fb9775aec6a815d37b3fbc142fea56fa5833e
Daz Jones <yuki@thebronasium.com>
2012-08-18 00:39:36 +0100

    Cater for Bluetooth hardcoding, remove some logging and partial cleanup

--- device.mk
+++ device.mk
@@ -71,8 +71,8 @@ PRODUCT_COPY_FILES += \
     device/huawei/u8818/prebuilt/system/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    device/huawei/u8818/prebuilt/system/etc/BCM4330.hcd:system/etc/BCM4330.hcd \
-    device/huawei/u8818/prebuilt/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
+    device/huawei/u8818/prebuilt/system/etc/bluetooth/BCM4330.hcd:system/etc/bluetooth/BCM4330.hcd \
+    device/huawei/u8818/prebuilt/system/etc/bluetooth/init.qcom.bt.sh:system/etc/bluetooth/init.qcom.bt.sh
 
 PRODUCT_COPY_FILES += \
     device/huawei/u8818/prebuilt/system/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
