28bf6381e7953aa5f5b772c1fbd725a8ccf0ab67
Daz Jones <yuki@thebronasium.com>
2012-08-21 17:12:17 +0100

    Need BOARD_WLAN_DEVICE := bcm4330 for brcm_patchram_plus to build.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,21 +73,19 @@ FM_RADIO := true
 BOARD_PROVIDES_LIBRIL := true
 
 # Wi-Fi
-BOARD_WLAN_DEVICE := bcmdhd
+BOARD_WLAN_DEVICE := bcm4330
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_EXT_MODULE_NAME := "cfg80211"
+WIFI_EXT_MODULE_PATH := "/system/lib/modules/cfg80211.ko"
+WIFI_DRIVER_MODULE_NAME := "dhd"
+WIFI_DRIVER_MODULE_PATH := "/system/wifi/dhd_4330.ko"
+WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/wifi/fw_4330_b2.bin nvram_path=/system/wifi/nvram_4330.txt"
 WIFI_DRIVER_FW_PATH_AP := "/system/wifi/fw_4330_b2.bin"
 WIFI_DRIVER_FW_PATH_STA := "/system/wifi/fw_4330_b2.bin"
 WIFI_DRIVER_FW_PATH_P2P := "/system/wifi/fw_4330_b2.bin"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_MODULE_PATH := "/system/wifi/dhd_4330.ko"
-WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/wifi/fw_4330_b2.bin nvram_path=/system/wifi/nvram_4330.txt"
-WIFI_DRIVER_MODULE_NAME := "dhd"
-WIFI_EXT_MODULE_PATH := "/system/lib/modules/cfg80211.ko"
-WIFI_EXT_MODULE_NAME := "cfg80211"
 
 # Kernel 
 TARGET_KERNEL_SOURCE := kernel/huawei/u8815
