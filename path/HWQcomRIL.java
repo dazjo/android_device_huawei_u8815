a035ad6bc6560278a21cba939e5e2efb378b4818
Daz Jones <yuki@thebronasium.com>
2012-10-15 00:10:02 +0100

    Now it should actually work :p

--- path/HWQcomRIL.java
+++ path/HWQcomRIL.java
@@ -51,12 +51,6 @@ public class HWQcomRIL extends QualcommSharedRIL implements CommandsInterface {
     boolean RILJ_LOGV = true;
     boolean RILJ_LOGD = true;
 
-    private final int RIL_INT_RADIO_OFF = 0;
-    private final int RIL_INT_RADIO_UNAVAILABLE = 1;
-    private final int RIL_INT_RADIO_ON = 2;
-    private final int RIL_INT_RADIO_ON_NG = 10;
-    private final int RIL_INT_RADIO_ON_HTC = 13;
-
     public HWQcomRIL(Context context, int networkMode, int cdmaSubscription) {
         super(context, networkMode, cdmaSubscription);
     }
@@ -275,48 +269,5 @@ public class HWQcomRIL extends QualcommSharedRIL implements CommandsInterface {
         }
 
         return ret;
-    }
-
-    private void setRadioStateFromRILInt(int stateCode) {
-        CommandsInterface.RadioState radioState;
-        HandlerThread handlerThread;
-        Looper looper;
-        IccHandler iccHandler;
-
-        switch (stateCode) {
-            case RIL_INT_RADIO_OFF:
-                radioState = CommandsInterface.RadioState.RADIO_OFF;
-                if (mIccHandler != null) {
-                    mIccThread = null;
-                    mIccHandler = null;
-                }
-                break;
-            case RIL_INT_RADIO_UNAVAILABLE:
-                radioState = CommandsInterface.RadioState.RADIO_UNAVAILABLE;
-                break;
-            case RIL_INT_RADIO_ON:
-            case RIL_INT_RADIO_ON_NG:
-            case RIL_INT_RADIO_ON_HTC:
-                if (mIccHandler == null) {
-                    handlerThread = new HandlerThread("IccHandler");
-                    mIccThread = handlerThread;
-
-                    mIccThread.start();
-
-                    looper = mIccThread.getLooper();
-                    mIccHandler = new IccHandler(this,looper);
-                    mIccHandler.run();
-                }
-                if (mPhoneType == RILConstants.CDMA_PHONE) {
-                    radioState = CommandsInterface.RadioState.RUIM_NOT_READY;
-                } else {
-                    radioState = CommandsInterface.RadioState.SIM_NOT_READY;
-                }
-                break;
-            default:
-                throw new RuntimeException("Unrecognized RIL_RadioState: " + stateCode);
-        }
-
-        setRadioState (radioState);
     }
 }
