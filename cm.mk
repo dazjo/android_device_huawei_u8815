83637c1861c298f62678eba03fa0435a2e11ab98
fagulhas <fagulhas@jellybean.(none)>
2012-10-16 22:12:28 +0100

    Inherit GSM tech before full_phone and device configuration after
    Fixes wrong apn-conf.xml being copied to system/etc.
    General comments added and cleanups.

--- cm.mk
+++ cm.mk
@@ -1,19 +1,25 @@
-# Inherit device configuration.
-$(call inherit-product, device/huawei/u8815/u8815.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := U8815
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/u8815/u8815.mk)
 
 # Correct boot animation size for the screen.
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Setup device configuration.
+# Device naming
 PRODUCT_NAME := cm_u8815
 PRODUCT_DEVICE := u8815
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Ascend G300
 PRODUCT_MANUFACTURER := HUAWEI
+
+# Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=U8815 BUILD_ID=JZO54K BUILD_FINGERPRINT=ZTE/N880E_JB/atlas40:4.1.1/JRO03C/eng.songsy.20120718.233441:eng/test-keys PRIVATE_BUILD_DESC="N880E_JB-eng 4.1.1 JRO03C eng.songsy.20120718.233441 test-keys" BUILD_NUMBER=eng.songsy.20120718.233441
-PRODUCT_RELEASE_NAME := U8815
