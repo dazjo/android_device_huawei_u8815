220dadd0940054777df35c401635642f8d723de7
Ricardo <ricardo.pinho.rp@gmail.com>
2012-10-22 22:57:52 +0100

    Second pass at a fully working CWM 6.x
    - Re-added rmt_storage service, fixes random recovery reboots
    - Soft keys light off for charge mode and on for recovery
    
    Issues to be fixed on third pass: Error Status 7 and Incorrect touchscreen/soft keys behaviour

--- recovery/sbin/charge.sh
+++ recovery/sbin/charge.sh
@@ -4,9 +4,15 @@
 cat /proc/app_info | grep -A1 "charge_flag:" | grep -q "1"
 recovery=$?
 
-# Don't start recovery if we're in charge mode.
+# Don't start recovery if we're in charge mode. Turn off soft keys light.
 if [ $recovery == 0 ] ; then
+ echo 0 > /sys/class/leds/button-backlight/brightness
  exit 0
+fi
+
+# Turn on soft keys light for recovery mode.
+if [ $recovery == 1 ] ; then
+ echo 255 > /sys/class/leds/button-backlight/brightness
 fi
 
 # We haven't exited, start recovery.
