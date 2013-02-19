3808f7e87892d80c96025b8ed76d74669c26a73a
Daz Jones <yuki@thebronasium.com>
2013-02-19 00:57:41 +0000

    Fix kernel building

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,13 +1,8 @@
+LOCAL_PATH := $(call my-dir)
 
-LOCAL_PATH := device/huawei/u8815/prebuilt
+include $(CLEAR_VARS)
 
-#
-# Kernel
-#
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
+# include the non-open-source counterpart to this file
+-include vendor/huawei/u8815/AndroidBoardVendor.mk
