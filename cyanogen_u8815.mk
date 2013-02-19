40d5f989b7da835d8435e6608c4fbd31b50659e8
Daz Jones <yuki@thebronasium.com>
2013-02-19 00:43:48 +0000

    cyanogen_u8815: fix ups

--- cyanogen_u8815.mk
+++ cyanogen_u8815.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/huawei/u8815/device_u8815.mk)
 
 # Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common_full_no_themes.mk)
+$(call inherit-product, vendor/cyanogen/products/common_full.mk)
 
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
@@ -20,13 +20,10 @@ PRODUCT_MODEL := U8815
 PRODUCT_MANUFACTURER := Huawei
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u8815 BUILD_ID=GRK39F BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=google/passion/passion:2.3.6/GRK39F/189904:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.6 GRK39F 189904 release-keys"
 
-# Extra LDPI overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
-
 # Release name and versioning
 PRODUCT_RELEASE_NAME := U8815
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cyanogen/products/common_versions.mk
 
 PRODUCT_COPY_FILES +=  \
-     vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
+     vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
