88c50c74f2ff1348d26bed5f6740e2d6d89d83c8
cicki <cickii@gmail.com>
2013-05-19 01:05:19 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,24 +2,24 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := U8815
+PRODUCT_RELEASE_NAME := U8655
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/huawei/u8815/u8815.mk)
+$(call inherit-product, device/huawei/u8655/u8655.mk)
 
 # Correct boot animation size for the screen.
-TARGET_BOOTANIMATION_NAME := vertical-480x800
+TARGET_BOOTANIMATION_NAME := vertical-320x480
 
 # Device naming
-PRODUCT_NAME := cm_u8815
-PRODUCT_DEVICE := u8815
+PRODUCT_NAME := cm_u8655
+PRODUCT_DEVICE := u8655
 PRODUCT_BRAND := Huawei
-PRODUCT_MODEL := Ascend G300
+PRODUCT_MODEL := Ascend Y200
 PRODUCT_MANUFACTURER := HUAWEI
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=U8815 BUILD_FINGERPRINT=Huawei/U8815/hwu8815:4.0.3/HuaweiU8815/C00B934:user/ota-rel-keys,release-keys PRIVATE_BUILD_DESC="U8815-user 4.0.3 GRJ90 C00B934 release-keys" BUILD_NUMBER=C00B934
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=U8655 BUILD_FINGERPRINT=Huawei/U8655/hwu8655:4.0.3/HuaweiU8655/C00B934:user/ota-rel-keys,release-keys PRIVATE_BUILD_DESC="U8655-user 4.0.3 GRJ90 C00B934 release-keys" BUILD_NUMBER=C00B934
 
