0ce459855168552cf58f7bd0a866dc66a3cde97e
cicki <cickii@gmail.com>
2013-05-19 01:02:13 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,14 +3,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The GPS configuration appropriate for this device.
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
-$(call inherit-product, vendor/huawei/u8815/u8815-vendor.mk)
+$(call inherit-product, vendor/huawei/u8655/u8655-vendor.mk)
 
 FRAMEWORKS_BASE_SUBDIRS += ../../$(LOCAL_PATH)/ril/
 
-DEVICE_PACKAGE_OVERLAYS += device/huawei/u8815/overlay
+DEVICE_PACKAGE_OVERLAYS += device/huawei/u8655/overlay
 
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
 
 ifdef HUAWEI_109808_BASEBAND
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -40,15 +40,15 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.primary.u8815 \
-    audio_policy.u8815 \
+    audio.primary.u8655 \
+    audio_policy.u8655 \
     audio.a2dp.default \
     libaudioutils
 
 # Other HALs
 PRODUCT_PACKAGES += \
-    gps.u8815
-#   lights.u8815
+    gps.u8655
+#   lights.u8655
 
 # Other Packages
 PRODUCT_PACKAGES += \
@@ -60,39 +60,39 @@ PRODUCT_PACKAGES += \
     com.android.future.usb.accessory 
 
 PRODUCT_COPY_FILES += \
-    device/huawei/u8815/ramdisk/init.huawei.rc:root/init.huawei.rc \
-    device/huawei/u8815/ramdisk/ueventd.huawei.rc:root/ueventd.huawei.rc \
-    device/huawei/u8815/ramdisk/init.huawei.usb.rc:root/init.huawei.usb.rc
+    device/huawei/u8655/ramdisk/init.huawei.rc:root/init.huawei.rc \
+    device/huawei/u8655/ramdisk/ueventd.huawei.rc:root/ueventd.huawei.rc \
+    device/huawei/u8655/ramdisk/init.huawei.usb.rc:root/init.huawei.usb.rc
 
 PRODUCT_COPY_FILES += \
-    device/huawei/u8815/prebuilt/fw_4330_b2.bin:system/etc/fw_4330_b2.bin \
-    device/huawei/u8815/configs/nvram_4330.txt:system/etc/nvram_4330.txt \
-    device/huawei/u8815/configs/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
-    device/huawei/u8815/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+    device/huawei/u8655/prebuilt/fw_4330_b2.bin:system/etc/fw_4330_b2.bin \
+    device/huawei/u8655/configs/nvram_4330.txt:system/etc/nvram_4330.txt \
+    device/huawei/u8655/configs/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/huawei/u8655/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    device/huawei/u8815/prebuilt/BCM4330.hcd:system/etc/bluetooth/BCM4330.hcd \
-    device/huawei/u8815/configs/init.bcm.bt.sh:system/etc/bluetooth/init.bcm.bt.sh
+    device/huawei/u8655/prebuilt/BCM4330.hcd:system/etc/bluetooth/BCM4330.hcd \
+    device/huawei/u8655/configs/init.bcm.bt.sh:system/etc/bluetooth/init.bcm.bt.sh
 
 PRODUCT_COPY_FILES += \
-    device/huawei/u8815/configs/audio_policy.conf:system/etc/audio_policy.conf \
-    device/huawei/u8815/configs/AudioFilter.csv:system/etc/AudioFilter.csv \
-    device/huawei/u8815/configs/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
-    device/huawei/u8815/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    device/huawei/u8815/configs/nfcee_access.xml:system/etc/nfcee_access.xml \
-    device/huawei/u8815/configs/libcm.sh:system/etc/libcm.sh \
-    device/huawei/u8815/configs/vold.fstab:system/etc/vold.fstab
+    device/huawei/u8655/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    device/huawei/u8655/configs/AudioFilter.csv:system/etc/AudioFilter.csv \
+    device/huawei/u8655/configs/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
+    device/huawei/u8655/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/huawei/u8655/configs/nfcee_access.xml:system/etc/nfcee_access.xml \
+    device/huawei/u8655/configs/libcm.sh:system/etc/libcm.sh \
+    device/huawei/u8655/configs/vold.fstab:system/etc/vold.fstab
 
 PRODUCT_COPY_FILES += \
-    device/huawei/u8815/idc/synaptics.idc:system/usr/idc/synaptics.idc \
-    device/huawei/u8815/idc/qwerty.idc:system/usr/idc/qwerty.idc \
-    device/huawei/u8815/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
-    device/huawei/u8815/keychars/7x27a_kp.kcm:system/usr/keychars/7x27a_kp.kcm \
-    device/huawei/u8815/keylayout/7x27a_kp.kl:system/usr/keylayout/7x27a_kp.kl \
-    device/huawei/u8815/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
-    device/huawei/u8815/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/huawei/u8815/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
-    device/huawei/u8815/keylayout/surf_keypad.kl:system/usr/keylayout/surf_keypad.kl
+    device/huawei/u8655/idc/synaptics.idc:system/usr/idc/synaptics.idc \
+    device/huawei/u8655/idc/qwerty.idc:system/usr/idc/qwerty.idc \
+    device/huawei/u8655/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
+    device/huawei/u8655/keychars/7x27a_kp.kcm:system/usr/keychars/7x27a_kp.kcm \
+    device/huawei/u8655/keylayout/7x27a_kp.kl:system/usr/keylayout/7x27a_kp.kl \
+    device/huawei/u8655/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
+    device/huawei/u8655/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/huawei/u8655/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
+    device/huawei/u8655/keylayout/surf_keypad.kl:system/usr/keylayout/surf_keypad.kl
 
 # Install the features available on this device.
 PRODUCT_COPY_FILES += \
@@ -114,6 +114,6 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 $(call inherit-product, build/target/product/full.mk)
 
-PRODUCT_NAME := huawei_u8815
-PRODUCT_DEVICE := u8815
+PRODUCT_NAME := huawei_u8655
+PRODUCT_DEVICE := u8655
 PRODUCT_BRAND := Huawei
