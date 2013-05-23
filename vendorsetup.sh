8dfbf0ab8ab2f81a1d074bc04909fc6086257445
Daz Jones <yuki@thebronasium.com>
2013-05-24 00:30:50 +0100

    Hack part 2: only run patches if JENKINS_HOME is set

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,5 @@
-# Run device patches on the tree. I'm lazy and this is easy for nightly builds.
-sh device/huawei/u8815/patches/apply.sh
+if [ ! -z "$JENKINS_HOME" ]
+then
+  # If we're Jenkins, run device patches on the tree. I'm lazy and this is easy for nightly builds.
+  sh device/huawei/u8815/patches/apply.sh
+fi
